6b93f9ed1efbd5af19ef4292af8049dbe66b423e
Michele Galvagno <michicello@gmail.com>
2019-04-20 21:38:45 +0200

    Initial Commit

---
+++ TextTransformer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TextTransformer
+//
+//  Created by Michele Galvagno on 20/04/2019.
+//  Copyright © 2019 Michele Galvagno. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
