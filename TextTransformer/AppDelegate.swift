8122621248f0de4a2a6a7e1e0d5fb72a7fed5dc4
Michele Galvagno <michicello@gmail.com>
2019-04-21 11:34:02 +0200

    Finished project

--- TextTransformer/AppDelegate.swift
+++ TextTransformer/AppDelegate.swift
@@ -10,15 +10,29 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        statusItem.button?.title = "𝒂"
+        statusItem.button?.target = self
+        statusItem.button?.action = #selector(showSettings)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+    }
+    
+    @objc func showSettings() {
+        let storyboard = NSStoryboard(name: "Main", bundle: nil)
+        guard let vc = storyboard.instantiateController(withIdentifier: "ViewController") as? ViewController else {
+            fatalError("Unable to find ViewController in the storyboard")
+        }
+        
+        let popoverView = NSPopover()
+        popoverView.contentViewController = vc
+        popoverView.behavior = .transient // when you click elsewhere it will go away
+        popoverView.show(relativeTo: statusItem.button!.bounds, of: statusItem.button!, preferredEdge: .maxY)
     }
 
 
---
+++ TextTransformer/ZalgoCharacters.swift
@@ -0,0 +1,23 @@
+//
+//  ZalgoCharacters.swift
+//  TextTransformer
+//
+//  Created by Michele Galvagno on 21/04/2019.
+//  Copyright © 2019 Michele Galvagno. All rights reserved.
+//
+
+import Foundation
+
+struct ZalgoCharacters: Codable {
+    let above: [String]
+    let inline: [String]
+    let below: [String]
+    
+    init() {
+        let url = Bundle.main.url(forResource: "zalgo", withExtension: "json")!
+        let data = try! Data(contentsOf: url)
+        
+        let decoder = JSONDecoder()
+        self = try! decoder.decode(ZalgoCharacters.self, from: data)
+    }
+}
